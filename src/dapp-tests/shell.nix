d9b1f0ad2cca34bb18f1ac4d6733a2d2c61a7bbb
Mate Soos <mate.soos@ethereum.org>
2022-09-01 18:37:03 +0200

    This should now work

--- src/dapp-tests/shell.nix
+++ src/dapp-tests/shell.nix
@@ -3,6 +3,14 @@
 }:
 
 with pkgs;
+let
+  my-python-packages = python-packages: with python-packages; [
+    hypothesis
+    pytest
+    # other python packages you want
+  ];
+  python-with-pkgs = python3.withPackages my-python-packages;
+in
 
 mkShell {
   name = "dapp-tests";
@@ -20,6 +28,7 @@ mkShell {
     jq
     killall
     procps
+    python-with-pkgs
     seth
     solc
     util-linux
