ef38d0fdba98c89df21aa13aed5b761599c322f1
Mate Soos <mate.soos@ethereum.org>
2022-09-15 18:15:00 +0200

    Fixing indentation

--- src/hevm/test/test.hs
+++ src/hevm/test/test.hs
@@ -406,13 +406,13 @@ tests = testGroup "hevm"
                 require(deposit_count < 2**32 - 1);
                 ++deposit_count;
                 bool found = false;
-                 for (uint height = 0; height < 32; height++) {
-                   if ((deposit_count & 1) == 1) {
-                     found = true;
-                     break;
-                   }
-                  deposit_count = deposit_count >> 1;
-                 }
+                for (uint height = 0; height < 32; height++) {
+                  if ((deposit_count & 1) == 1) {
+                    found = true;
+                    break;
+                  }
+                 deposit_count = deposit_count >> 1;
+                }
                 assert(found);
               }
              }
