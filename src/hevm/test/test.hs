e7738fb9d67f53bc5316252ac415a1cb936a99da
Mate Soos <mate.soos@ethereum.org>
2022-09-15 16:20:55 +0200

    This test now passes

--- src/hevm/test/test.hs
+++ src/hevm/test/test.hs
@@ -372,7 +372,7 @@ tests = testGroup "hevm"
           [Cex _, Cex _] <- withSolvers Z3 1 $ \s -> checkAssert s defaultPanicCodes c (Just ("fun(uint256)", [AbiUIntType 256])) []
           putStrLn "expected 2 counterexamples found"
         ,
-        expectFail $ testCase "assert-fail-notequal" $ do
+        testCase "assert-fail-notequal" $ do
           Just c <- solcRuntime "AssertFailNotEqual"
             [i|
             contract AssertFailNotEqual {
