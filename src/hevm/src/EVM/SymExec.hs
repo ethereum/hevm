f3a27d810cf1a4524708bb2d1ffcde91765c1188
zoep <zoe.paraskevopoulou@gmail.com>
2022-12-14 19:33:56 +0200

    SymExec: add error back

--- src/hevm/src/EVM/SymExec.hs
+++ src/hevm/src/EVM/SymExec.hs
@@ -366,6 +366,7 @@ flattenExpr = go []
       e@(Return _ _ _) -> [(pcs, e)]
       Failure _ (TmpErr s) -> error s
       e@(Failure _ _) -> [(pcs, e)]
+
       GVar _ -> error "cannot flatten an Expr containing a GVar"
 
 -- | Strips unreachable branches from a given expr
