db88682a35d2393b101d1f5fd48ffa6ec5f349d0
David Terry <me@xwvvvvwx.com>
2022-12-15 20:20:47 +0100

    rpc-tests: symexec for remote WETH

--- src/hevm/test/rpc.hs
+++ src/hevm/test/rpc.hs
@@ -3,14 +3,19 @@
 
 module Main where
 
-import Data.Text (Text)
-
+import Control.Lens
 import Test.Tasty
 import Test.Tasty.HUnit
+import Data.Text (Text)
+import qualified Data.Text.IO as T
 
-import qualified EVM.Fetch as Fetch
-import qualified EVM
-import EVM.Types
+import EVM
+import EVM.ABI
+import EVM.SMT
+import EVM.Format
+import EVM.Fetch
+import EVM.SymExec
+import EVM.Types hiding (BlockNumber)
 
 main :: IO ()
 main = defaultMain tests
@@ -19,8 +24,8 @@ tests :: TestTree
 tests = testGroup "rpc"
   [ testGroup "Block Parsing Tests"
     [ testCase "pre-merge-block" $ do
-        let block' = Fetch.BlockNumber 15537392
-        (cb, numb, basefee, prevRan) <- Fetch.fetchBlockFrom block' testRpc >>= \case
+        let block' = BlockNumber 15537392
+        (cb, numb, basefee, prevRan) <- fetchBlockFrom block' testRpc >>= \case
                       Nothing -> error "Could not fetch block"
                       Just EVM.Block{..} -> return (_coinbase
                                                    , _number
@@ -29,12 +34,12 @@ tests = testGroup "rpc"
                                                    )
 
         assertEqual "coinbase" (Addr 0xea674fdde714fd979de3edf0f56aa9716b898ec8) cb
-        assertEqual "number" (Fetch.BlockNumber numb) block'
+        assertEqual "number" (BlockNumber numb) block'
         assertEqual "basefee" 38572377838 basefee
         assertEqual "prevRan" 11049842297455506 prevRan
     , testCase "post-merge-block" $ do
-        let block' = Fetch.BlockNumber 16184420
-        (cb, numb, basefee, prevRan) <- Fetch.fetchBlockFrom block' testRpc >>= \case
+        let block' = BlockNumber 16184420
+        (cb, numb, basefee, prevRan) <- fetchBlockFrom block' testRpc >>= \case
                       Nothing -> error "Could not fetch block"
                       Just EVM.Block{..} -> return (_coinbase
                                                    , _number
@@ -43,11 +48,63 @@ tests = testGroup "rpc"
                                                    )
 
         assertEqual "coinbase" (Addr 0x690b9a9e9aa1c9db991c7721a92d351db4fac990) cb
-        assertEqual "number" (Fetch.BlockNumber numb) block'
+        assertEqual "number" (BlockNumber numb) block'
         assertEqual "basefee" 22163046690 basefee
         assertEqual "prevRan" 0x2267531ab030ed32fd5f2ef51f81427332d0becbd74fe7f4cd5684ddf4b287e0 prevRan
     ]
+  , testGroup "execution with remote state"
+    [ testCase "dapp-test" undefined
+    , testCase "weth-conc" undefined
+    , testCase "weth-sym" $ do
+        let
+          blockNum = 16191378
+          calldata' = symCalldata "transfer" [AbiAddressType, AbiUIntType 256] ["0xdead"] (AbstractBuf "txdata")
+          postc _ (EVM.Types.Revert _ _) = PBool False
+          postc _ _ = PBool True
+        vm <- vmFromRpc blockNum calldata' (CallValue 0) (Lit 0xf04a5cc80b1e94c69b48f5ee68a08cd2f09a7c3e) (Addr 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)
+        (expr, res) <- withSolvers Z3 1 Nothing $ \solvers ->
+          verify solvers ((rpcVeriOpts (BlockNumber blockNum, testRpc)) {debug = True}) vm (Just postc)
+        print res
+        T.putStrLn $ formatExpr expr
+        assertBool "" False
+    ]
   ]
+
+vmFromRpc :: W256 -> (Expr Buf, [Prop]) -> Expr EWord -> Expr EWord -> Addr -> IO (EVM.VM)
+vmFromRpc blockNum calldata' callvalue' caller' address' = do
+  ctrct <- fetchContractFrom (BlockNumber blockNum) testRpc address' >>= \case
+        Nothing -> error $ "contract not found: " <> show address'
+        Just contract' -> return contract'
+
+  blk <- fetchBlockFrom (BlockNumber blockNum) testRpc >>= \case
+    Nothing -> error "could not fetch block"
+    Just b -> pure b
+
+  pure $ EVM.makeVm $ EVM.VMOpts
+    { EVM.vmoptContract      = ctrct
+    , EVM.vmoptCalldata      = calldata'
+    , EVM.vmoptValue         = callvalue'
+    , EVM.vmoptAddress       = address'
+    , EVM.vmoptCaller        = caller'
+    , EVM.vmoptOrigin        = 0xacab
+    , EVM.vmoptGas           = 0xffffffffffffffff
+    , EVM.vmoptGaslimit      = 0xffffffffffffffff
+    , EVM.vmoptBaseFee       = view baseFee blk
+    , EVM.vmoptPriorityFee   = 0
+    , EVM.vmoptCoinbase      = view coinbase blk
+    , EVM.vmoptNumber        = view number blk
+    , EVM.vmoptTimestamp     = view timestamp blk
+    , EVM.vmoptBlockGaslimit = view gaslimit blk
+    , EVM.vmoptGasprice      = 0
+    , EVM.vmoptMaxCodeSize   = view maxCodeSize blk
+    , EVM.vmoptPrevRandao    = view prevRandao blk
+    , EVM.vmoptSchedule      = view schedule blk
+    , EVM.vmoptChainId       = 1
+    , EVM.vmoptCreate        = False
+    , EVM.vmoptStorageBase   = EVM.Concrete
+    , EVM.vmoptTxAccessList  = mempty
+    , EVM.vmoptAllowFFI      = False
+    }
 
 testRpc :: Text
 testRpc = "https://eth-mainnet.alchemyapi.io/v2/vpeKFsEF6PHifHzdtcwXSDbhV3ym5Ro4"
