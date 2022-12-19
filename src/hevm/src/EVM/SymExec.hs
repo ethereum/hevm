72fc2c48f8c54ce0d7c84ed173cb6c95fa0aff2d
zoep <zoe.paraskevopoulou@gmail.com>
2022-12-19 22:05:19 +0200

    SymExec: handle TmpErr and fix argument order

--- src/hevm/src/EVM/SymExec.hs
+++ src/hevm/src/EVM/SymExec.hs
@@ -528,7 +528,9 @@ equivalenceCheck solvers bytecodeA bytecodeB opts signature' = do
             (Revert _ a, Revert _ b) -> if a==b then PBool False else a ./= b
             (Revert _ _, _) -> PBool True
             (_, Revert _ _) -> PBool True
-            (Failure erra _, Failure errb _) -> if erra==errb then PBool False else PBool True
+            (Failure _ (TmpErr s), _) -> error $ "Unhandled error: " <> s
+            (_, Failure _ (TmpErr s)) -> error $ "Unhandled error: " <> s
+            (Failure _ erra, Failure _ errb) -> if erra==errb then PBool False else PBool True
             (ITE _ _ _, _ ) -> error "Expressions must be flattened"
             (_, ITE _ _ _) -> error "Expressions must be flattened"
 
