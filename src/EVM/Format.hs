a1d4e790320eceaf270b04674f8c72c8bb5949e7
Mate Soos <mate.soos@ethereum.org>
2023-01-05 18:42:36 +0100

    Fixing build

--- src/EVM/Format.hs
+++ src/EVM/Format.hs
@@ -379,6 +379,7 @@ prettyError= \case
   EVM.Types.StackLimitExceeded -> "Stack limit exceeded"
   EVM.Types.InvalidMemoryAccess -> "Invalid memory access"
   EVM.Types.BadJumpDestination -> "Bad jump destination"
+  EVM.Types.StackUnderrun -> "Stack underrun"
   TmpErr err -> "Temp error: " <> err
 
 
