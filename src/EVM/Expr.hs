009fb0a9188d306434f61a876decb0e2d8d25370
zoep <zoe.paraskevopoulou@gmail.com>
2023-02-10 18:01:59 +0200

    Expr: use op2 in min and max, fix max implementation

--- src/EVM/Expr.hs
+++ src/EVM/Expr.hs
@@ -959,12 +959,10 @@ eqByte (LitByte x) (LitByte y) = Lit $ if x == y then 1 else 0
 eqByte x y = EqByte x y
 
 min :: Expr EWord -> Expr EWord -> Expr EWord
-min (Lit x) (Lit y) = if x < y then Lit x else Lit y
-min x y = Min x y
+min x y = op2 Min Prelude.min x y
 
 max :: Expr EWord -> Expr EWord -> Expr EWord
-max (Lit x) (Lit y) = if x > y then Lit x else Lit y
-max x y = Min x y
+max x y = op2 Max Prelude.max x y
 
 numBranches :: Expr End -> Int
 numBranches (ITE _ t f) = numBranches t + numBranches f
