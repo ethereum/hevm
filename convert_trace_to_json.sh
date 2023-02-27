b4775a41fcb69601147085e8a2d0275320280e38
Mate Soos <mate.soos@ethereum.org>
2023-02-27 14:21:27 +0100

    Fixing bash usage in script

--- convert_trace_to_json.sh
+++ convert_trace_to_json.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 num=$(wc -l "$1" | cut -d " " -f 1)
 awk -v ll="$num" 'BEGIN {print "{\"trace\": [" } {if (NR!=ll) {print $0; if (NR!=ll-1) {print ","}} else {print "], \"output\":" $0}} END{print "}"}' "$1" > "$1.json"
