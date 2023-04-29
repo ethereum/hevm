7750a22b1145b6d4305c4b6c5288381072712a74
dxo <6689924+d-xo@users.noreply.github.com>
2023-04-29 12:18:45 +0200

    Update src/EVM.hs
    Co-authored-by: Zoe Paraskevopoulou <zoe.paraskevopoulou@gmail.com>

--- src/EVM.hs
+++ src/EVM.hs
@@ -218,8 +218,8 @@ exec1 = do
                   touchAccount self
                   out <- use (#state % #returndata)
                   finishFrame (FrameReturned out)
-              e -> partial
-                (UnexpectedSymbolicArg vmx.state.pc "precompile returned a symbolic value" (wrap [e]))
+              e -> partial $
+                     UnexpectedSymbolicArg vmx.state.pc "precompile returned a symbolic value" (wrap [e])
             _ ->
               underrun
       e -> partial $
