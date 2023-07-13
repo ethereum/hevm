6a5623c16ec13813b933d6e13b9e365a7df9fbf0
David Terry <me@xwvvvvwx.com>
2023-07-13 18:10:12 +0200

    SMT: fix typo

--- src/EVM/SMT.hs
+++ src/EVM/SMT.hs
@@ -480,7 +480,7 @@ prelude =  (flip SMT2) mempty $ fmap (fromLazyText . T.drop 2) . T.lines $ [i|
   (define-const emptyBuf Buf ((as const Buf) #b00000000))
 
   ; stores
-  (define-const emptyBuf Store ((as const Store) #x0000000000000000000000000000000000000000000000000000000000000000))
+  (define-const emptyStore Store ((as const Store) #x0000000000000000000000000000000000000000000000000000000000000000))
 
   (define-fun readWord ((idx Word) (buf Buf)) Word
     (concat
