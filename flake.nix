161a3ace22b0798c7670a2f55e96cbc8299437de
Artur Cygan <arczicygan@gmail.com>
2024-03-21 11:25:27 +0100

    Fix tests

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
             (haskell.lib.compose.addTestToolDepends testDeps)
             (haskell.lib.compose.appendBuildFlags ["-v3"])
             (haskell.lib.compose.appendConfigureFlags (
-              [ "-fci"
+              [ # "-fci"
                 "-O2"
               ]
               ++ lib.optionals stdenv.isDarwin
@@ -171,8 +171,8 @@
             packages = _: [ hevmUnwrapped ];
             buildInputs = [
               # cabal from nixpkgs
-              # haskellPackages.cabal-install
-              cabal-multi-pkgs.cabal-install
+              haskellPackages.cabal-install
+              # cabal-multi-pkgs.cabal-install
               mdbook
               yarn
               haskellPackages.eventlog2html
