27b1f45f0f0989fe3802ced7bab2b0eb78251524
dxo <hi@d-xo.org>
2024-04-08 12:42:05 +0200

    Solidity: ignore some more kontrol related build artifacts

--- src/EVM/Solidity.hs
+++ src/EVM/Solidity.hs
@@ -309,7 +309,8 @@ readBuildOutput root _ = do
 
 -- | Finds all json files under the provided filepath, searches recursively
 findJsonFiles :: FilePath -> IO [FilePath]
-findJsonFiles root =  filter (not . isInfixOf "kompiled") -- HACK: this gets added to `out` by `kontrol`
+findJsonFiles root =  filter (not . isInfixOf "kompiled")   -- HACK: this gets added to `out` by `kontrol`
+                  <$> filter (not . isInfixOf "build-info") -- HACK: this gets added to `out` by `kontrol`
                   <$> getDirectoryFiles root ["**/*.json"]
 
 -- | Filters out metadata json files
