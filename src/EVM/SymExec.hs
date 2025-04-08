32b9ed9d6386faa05f4bf45fe7853a3f7f2710a6
Mate Soos <mate.soos@argot.org>
2025-04-08 12:03:18 +0200

    Renaming

--- src/EVM/SymExec.hs
+++ src/EVM/SymExec.hs
@@ -325,13 +325,13 @@ interpret fetcher maxIter askSmtIters heuristic vm =
         when (length vals < 2) $ internalError "PleaseRunAll requires at least 2 branches"
         frozen <- liftIO $ stToIO $ freezeVM vm
         let newDepth = vm.exploreDepth+1
-        threads <- withRunInIO $ \runInIO -> mapConcurrently (runInIO . runOne frozen newDepth) vals
-        pure $ goITE (zip vals threads)
+        ends <- withRunInIO $ \runInIO -> mapConcurrently (runInIO . runOne frozen newDepth) vals
+        pure $ goITE (zip vals ends)
         where
           goITE :: [(W256, Expr End)] -> Expr End
           goITE [] = internalError "goITE: empty list"
-          goITE [(_, e)] = e
-          goITE ((v,e):ps) = ITE (Eq expr (Lit v)) e (goITE ps)
+          goITE [(_, end)] = end
+          goITE ((val,end):ps) = ITE (Eq expr (Lit val)) end (goITE ps)
           runOne :: App m => VM 'Symbolic RealWorld -> Int -> W256 -> m (Expr 'End)
           runOne frozen newDepth v = do
             (ra, vma) <- liftIO $ stToIO $ runStateT (continue v) frozen { result = Nothing, exploreDepth = newDepth }
