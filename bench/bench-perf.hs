084f22d35554a87657c679c40e169d4416bb7be0
Emilio López <emilio.lopez@trailofbits.com>
2025-05-16 12:07:33 +0200

    bench-perf: remove unused code

--- bench/bench-perf.hs
+++ bench/bench-perf.hs
@@ -4,40 +4,25 @@
 module Main where
 
 import Control.Monad.State.Strict (liftIO)
-import Data.Aeson qualified as JSON
 import Data.ByteString.UTF8 as BSU 
-import Data.ByteString qualified as ByteString
 import Data.Either
 import Data.Function
-import Data.Map.Strict qualified as Map
 import Data.Maybe
 import Data.String.Here
 import EVM
 import EVM.ABI
 import EVM.Effects (App, runApp)
 import EVM.Fetch qualified as Fetch
-import EVM.Format
-import EVM.Sign
 import EVM.Solidity
 import EVM.Stepper qualified as Stepper
-import EVM.Test.Tracing
 import EVM.Transaction qualified
 import EVM.Types
 import EVM.UnitTest
-import GHC.Word
-import System.Directory (setCurrentDirectory)
-import System.FilePath.Find qualified as Find
-import System.IO.Temp
-import System.Process (readProcessWithExitCode)
 import Test.Tasty.Bench
 import Control.Monad.ST
 import EVM.FeeSchedule (feeSchedule)
 
--- benchmark hevm and EVM tool on the folder called benchmarks
--- using tasty-bench
-
-vmFromByteString :: App m => ByteString -> m (VM Concrete RealWorld)
-vmFromByteString = vmFromRawByteString . fromJust . hexByteString
+-- benchmark hevm using tasty-bench
 
 vmFromRawByteString :: App m => ByteString -> m (VM Concrete RealWorld)
 vmFromRawByteString bs = liftIO $
@@ -83,21 +68,6 @@ vm0Opts c =
       beaconRoot = 0
     }
 
-benchCode :: ByteString -> Benchmark
-benchCode bs = bench "interpret" $ nfIO $ runApp $ do
-  vm <- vmFromByteString bs
-  isRight <$> Stepper.interpret (Fetch.zero 0 Nothing) vm Stepper.execFully
-
-benchFile :: FilePath -> Benchmark
-benchFile path = bench (show path) $ nfIO $ runApp $ do
-  bs <- liftIO $ strip0x <$> ByteString.readFile path
-  vm <- vmFromByteString bs
-  isRight <$> Stepper.interpret (Fetch.zero 0 Nothing) vm Stepper.execFully
-
--- bench every .bin file in a given folder
-benchFolder :: FilePath -> IO [Benchmark]
-benchFolder path = map benchFile <$> Find.find Find.always (Find.extension Find.==? ".bin") path
-
 -- why do we need this?
 vmOptsToTestVMParams :: VMOpts Concrete -> TestVMParams
 vmOptsToTestVMParams v =
@@ -133,109 +103,6 @@ callMainForBytecode bs = do
 
 benchMain :: (String, ByteString) -> Benchmark
 benchMain (name, bs) = bench name $ nfIO $ runApp $ (\x -> if isRight x then () else internalError "failed") <$> callMainForBytecode bs
-
-benchBytecodes :: [ByteString] -> [Benchmark]
-benchBytecodes = map (\x -> bench "bytecode" $ nfIO $ runApp (do
-                                                       vm <- vmFromRawByteString x
-                                                       isRight <$> (Stepper.interpret (Fetch.zero 0 Nothing) vm Stepper.execFully)))
-
-callMain :: TestVMParams -> EVM Concrete s ()
-callMain params = makeTxCall params (ConcreteBuf (abiMethod "main()" emptyAbi), [])
-
-evmSetup' :: [Word8] -> ByteString -> (EVM.Transaction.Transaction, EVMToolEnv, EVMToolAlloc, Addr, Addr, Integer)
-evmSetup' contr txData = (txn, evmEnv, contrAlloc, fromAddress, toAddress, sk)
-  where
-    bitcode = ByteString.pack contr
-    contrAlloc =
-      EVMToolAlloc
-        { balance = 0xffffffffffffff,
-          code = bitcode,
-          nonce = 0x48
-        }
-    txn =
-      EVM.Transaction.Transaction
-        { txdata = txData,
-          gasLimit = 0xffffffffffffff,
-          gasPrice = Just 1,
-          nonce = 172,
-          toAddr = Just 0x8A8eAFb1cf62BfBeb1741769DAE1a9dd47996192,
-          r = 0, -- will be fixed when we sign
-          s = 0, -- will be fixed when we sign
-          v = 0, -- will be fixed when we sign
-          value = 0, -- setting this > 0 fails because HEVM doesn't handle value sent in toplevel transaction
-          txtype = EVM.Transaction.EIP1559Transaction,
-          accessList = [],
-          maxPriorityFeeGas = Just 1,
-          maxFeePerGas = Just 1,
-          chainId = 1
-        }
-    evmEnv =
-      EVMToolEnv
-        { coinbase = 0xff,
-          timestamp = Lit 0x3e8,
-          number = Lit 0x0,
-          gasLimit = 0xffffffffffffff,
-          baseFee = 0x0,
-          maxCodeSize = 0xfffff,
-          schedule = feeSchedule,
-          blockHashes = blockHashesDefault,
-          withdrawals = mempty,
-          currentRandom = 42,
-          parentBeaconBlockRoot = 5
-        }
-    sk = 0xDC38EE117CAE37750EB1ECC5CFD3DE8E85963B481B93E732C5D0CB66EE6B0C9D
-    fromAddress :: Addr
-    fromAddress = fromJust $ deriveAddr sk
-    toAddress :: Addr
-    toAddress = 0x8A8eAFb1cf62BfBeb1741769DAE1a9dd47996192
-
-runGethOnFile :: FilePath -> IO Benchmark
-runGethOnFile path = do
-  bs <- fromJust . hexByteString . strip0x <$> ByteString.readFile path
-  let contr = ByteString.unpack bs
-  let (txn, evmEnv, contrAlloc, fromAddress, toAddress, sk) = evmSetup' contr mempty
-      txs = [EVM.Transaction.sign sk txn]
-      walletAlloc =
-        EVMToolAlloc
-          { balance = 0x5ffd4878be161d74,
-            code = mempty,
-            nonce = 0xac
-          }
-      alloc :: Map.Map Addr EVMToolAlloc
-      alloc = Map.fromList ([(fromAddress, walletAlloc), (toAddress, contrAlloc)])
-
-  -- do each benchmark in a temporary directory
-  pure $
-    bench (show path) $
-      nfIO $
-        withTempDirectory
-          "/tmp"
-          "name"
-          ( \dir -> do
-              setCurrentDirectory dir
-              JSON.encodeFile "txs.json" txs
-              JSON.encodeFile "alloc.json" alloc
-              JSON.encodeFile "env.json" evmEnv
-              readProcessWithExitCode
-                "evm"
-                [ "transition",
-                  "--input.alloc",
-                  "alloc.json",
-                  "--input.env",
-                  "env.json",
-                  "--input.txs",
-                  "txs.json"
-                ]
-                ""
-          )
-
-benchEVMTool :: FilePath -> IO Benchmark
-benchEVMTool path = runGethOnFile path
-
-benchEVMToolFolder :: FilePath -> IO [Benchmark]
-benchEVMToolFolder path = do
-  files <- Find.find Find.always (Find.extension Find.==? ".bin") path
-  mapM benchEVMTool files
 
 mkBench :: (Int -> IO a) -> [Int] -> IO [(String, a)]
 mkBench f l = mapM (\n -> (show n,) <$> f n) l
